54dac99c7cc2ecbae7a7312fcb10de5d7b4bf221
Ben Koop <benjamin@smartrise.us>
2017-11-30 08:01:35 -0800

    added more function calls

--- Arcade/Intro.js
+++ Arcade/Intro.js
@@ -3,3 +3,24 @@ function longestWord(text) {
         return prev.length < current.length? 1: -1;
     })[0];
 }
+
+function validTime(time) {
+    time = time.split(':');
+    if (time[0] > 23) {
+        return false;
+    } else if (time[1] > 59) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function sumUpNumbers(inputString) {
+    try {
+    return parseInt(inputString.match(/(\d+)/g).reduce(function (acc, cv) {
+        return (parseInt(acc)+parseInt(cv));
+    }));
+    } catch (e) {
+        return 0;
+    }
+}
